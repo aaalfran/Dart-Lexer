14df2d3be4635a57d0c2e51c423bc70669565635
aaalfran <aaalfran@espol.edu.ec>
2022-11-12 13:04:21 -0500

    Creacion del virtual enviroment con liberia ply, se agregaron las primeras palabras resevadas

---
+++ lexico.py
@@ -0,0 +1,17 @@
+import ply.lex as lex
+
+# Aporte Aaron Franco
+
+# Inicio de palabras reservadas
+reserved = {
+# Tipado
+"var":"VAR",
+"String":"STRING",
+"int":"INT",
+# Estructuras de Control
+"if":"IF",
+"else":"ELSE",
+"while": "WHILE",
+"for":"FOR",
+
+}
